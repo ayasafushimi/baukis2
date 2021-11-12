d546715b969cc2b7ad18cb9d16a8af9afdc45177
ayasafushimi <bolbus0617@gmail.com>
2021-11-12 14:46:19 +0900

    14.1 終了

---
+++ app/models/concerns/string_normalizer.rb
@@ -0,0 +1,20 @@
+require "nkf"
+
+module StringNormalizer
+    extend ActiveSupport::Concern
+
+    def normalize_as_email(text)
+        # 全角英数字と記号とスペースを半角に変換し、先頭と末尾の空白を除去している。
+        NKF.nkf("-W -w -Z1", text).strip if text
+    end
+
+    def normalize_as_name(text)
+        # 全角英数字と記号とスペースを半角に変換し、先頭と末尾の空白を除去している。
+        NKF.nkf("-W -w -Z1", text).strip if text
+    end
+        # 全角英数字と記号とスペースを半角に変換し、先頭と末尾の空白を除去している。
+        # ひらがなをカタカナに変換している。
+    def normalize_as_furigana(text)
+        NKF.nkf("-W -w -Z1 --katakana", text).strip if text
+    end
+end
