d36aaf69e3ec065671949cb2b5e5e8dd627baf31
ayasafushimi <bolbus0617@gmail.com>
2021-11-17 18:35:42 +0900

    15 終了

--- app/presenters/model_presenter.rb
+++ app/presenters/model_presenter.rb
@@ -1,9 +1,11 @@
 class ModelPresenter
     # モデルを対象としたプレゼンテーター
 
+    # HTML生成モジュールを導入
     include HtmlBuilder
 
     attr_reader :object, :view_context
+
     # メソッドをview_contextに委譲する。
     delegate :raw, :link_to, to: :view_context
 
