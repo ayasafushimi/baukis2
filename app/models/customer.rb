c34b4537edbc0581e679746e313d787a7d3428c1
ayasafushimi <bolbus0617@gmail.com>
2021-11-24 16:52:50 +0900

    18.3.2 終了

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,6 +6,10 @@ class Customer < ApplicationRecord
     # autosave:trueを設定すると、関連づけられたオブジェクトもsaveされる。
     has_one :home_address, dependent: :destroy, autosave: true
     has_one :work_address, dependent: :destroy, autosave: true
+    has_many :phones, dependent: :destroy
+    # 第二引数にスコープ（付帯条件）を設定する。
+    has_many :personal_phones, -> {where(address_id: nil).order(:id)},
+        class_name: "Phone", autosave: true
 
     # inclusionは、値が特定のリストの中にあることを確かめる。
     validates :gender, inclusion: { in: %w(male female), allow_blank: true }
